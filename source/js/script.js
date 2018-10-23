23f53c005291076eaa037295017452e0142494f4
Varvara <sadchikhina@gmail.com>
2018-10-23 15:10:00 +0300

    js corrected

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var elements = document.getElementsByClassName("inner-wrapper");
-for (var i = 0; i < elements.length; i++) {/*прокручиваем в цикле все элементы*/
-  elements[i].addEventListener('click', function() {  /*при клике на элемент
- */
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].classList.remove('catalog__inner-wrapper--selected'); /*удаляем у всех class active*/
+var elements = document.getElementsByClassName('catalog__inner-wrapper');
+for (var i = 0; i < elements.length; i++) { /*прокручиваем в цикле все элементы*/
+  elements[i].addEventListener('click', function() {  /*при клике на элемент*/
+    if (elements[i].hasClass('catalog__inner-wrapper--selected') { /*удалить класс при повторном клике на элемент*/
+      elements[i].classList.remove('catalog__inner-wrapper--selected');
+    } else {
+      elements[i].classList.add('catalog__inner-wrapper--selected');/*добавляем class selected по которому кликнули */
     }
-    this.classList.add('catalog__inner-wrapper--selected');/*добавляем class active по которому кликнули */
   })
 }
